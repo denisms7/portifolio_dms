b69002e027d3a3148db65d5613d94ed084979069
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2025-11-20 16:35:22 -0300

    fixed data

--- static/js/datatables/conf.js
+++ static/js/datatables/conf.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.tabela-classificacao').DataTable({
-        order: [[0, 'desc']], // ordena pela 1ª coluna (Ano)
+        order: [[0, 'desc']],
         paging: false,
         searching: false,
         info: false,
@@ -8,7 +8,8 @@ $(document).ready(function () {
             url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/pt-BR.json'
         },
         columnDefs: [
-            { orderable: false, targets: -1 } // desativa ordenação da última coluna
+            { orderable: false, targets: -1 },
+            { type: 'string', targets: 0 } // força ordenação textual
         ]
     });
 });
